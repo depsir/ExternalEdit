cbee74ce0809b09db5ce8745c1d190eca5e78ae3
Francesco Sacchi <depsir@gmail.com>
2012-08-28 00:08:17 +0200

    Added command for loading file

--- externaledit.py
+++ externaledit.py
@@ -19,6 +19,41 @@ class ExternalEdit:
 	wk_url = None
 	pagename = None
 
+	def load(self, view):
+		wk_type = view.find('Type=.*',0)
+		wk_engine = view.find('Engine=.*',0)
+		wk_script = view.find('Script=.*',0)
+		wk_server = view.find('Server=.*',0)
+		wk_path = view.find('Path=.*',0)
+		wk_url = view.find('URL=.*',0)
+		if view.find('[Process]',0) and \
+			view.find('[File]',0) and \
+			wk_type and \
+			wk_engine and \
+			wk_script and \
+			wk_server and \
+			wk_path and \
+			wk_url:
+
+			wk_type= view.substr(sublime.Region(wk_type.a + 5, wk_type.b))
+			wk_engine = view.substr(sublime.Region(wk_engine.a + 7, wk_engine.b))
+			wk_script = view.substr(sublime.Region(wk_script.a + 7, wk_script.b))
+			wk_server = view.substr(sublime.Region(wk_server.a + 7, wk_server.b))
+			wk_path = view.substr(sublime.Region(wk_path.a + 5, wk_path.b))
+			wk_url = view.substr(sublime.Region(wk_url.a + 4, wk_url.b))
+			self.site = wiki.Wiki(wk_server+wk_path+"/api.php") 
+			pagename = re.search('title=([^&]*)', wk_url)
+			pagename = pagename.group(1)
+			view.set_name(pagename)
+			self.mypage = page.Page(self.site, pagename)
+			edit = view.begin_edit()
+			reg = sublime.Region(0,view.size())
+			view.erase(edit,reg)
+			view.set_syntax_file('Packages/Mediawiki.tmbundle/Syntaxes/Mediawiki.tmLanguage')
+			view.set_encoding('utf-8')
+			view.insert(edit,0,self.mypage.getWikiText().decode('utf-8'))
+			view.end_edit(edit)
+
 ee = ExternalEdit()
 
 class SendChanges(sublime_plugin.TextCommand):
@@ -30,57 +65,14 @@ class SendChanges(sublime_plugin.TextCommand):
 			ee.mypage.edit(self.view.substr(sublime.Region(0,self.view.size())))
 			print "saved"
 
+class eeProcessControlFile(sublime_plugin.TextCommand):
+	def run(self, edit):
+		global ee
+		ee.load(self.view)
+
 class EEEventListener(sublime_plugin.EventListener):
 	def on_load(self,view):
-		# try:
+		global ee
 		doc = view.file_name()
 		if re.search(r'index( *\([0-9]*\))*.php$',doc):
-			wk_type = view.find('Type=.*',0)
-			wk_engine = view.find('Engine=.*',0)
-			wk_script = view.find('Script=.*',0)
-			wk_server = view.find('Server=.*',0)
-			wk_path = view.find('Path=.*',0)
-			wk_url = view.find('URL=.*',0)
-			if view.find('[Process]',0) and \
-				view.find('[File]',0) and \
-				wk_type and \
-				wk_engine and \
-				wk_script and \
-				wk_server and \
-				wk_path and \
-				wk_url:
-				global ee
-				# print(doc)
-				# print view.substr(wk_type)
-				wk_type= view.substr(sublime.Region(wk_type.a + 5, wk_type.b))
-				wk_engine = view.substr(sublime.Region(wk_engine.a + 7, wk_engine.b))
-				wk_script = view.substr(sublime.Region(wk_script.a + 7, wk_script.b))
-				wk_server = view.substr(sublime.Region(wk_server.a + 7, wk_server.b))
-				wk_path = view.substr(sublime.Region(wk_path.a + 5, wk_path.b))
-				wk_url = view.substr(sublime.Region(wk_url.a + 4, wk_url.b))
-				ee.site = wiki.Wiki(wk_server+wk_path+"/api.php") 
-				pagename = re.search('title=([^&]*)', wk_url)
-				pagename = pagename.group(1)
-				view.set_name(pagename)
-				# print pagename
-				ee.mypage = page.Page(ee.site, pagename)
-				# print mypage.getWikiText()
-				edit = view.begin_edit()
-				reg = sublime.Region(0,view.size())
-				view.erase(edit,reg)
-				view.set_syntax_file('Packages/Mediawiki.tmbundle/Syntaxes/Mediawiki.tmLanguage')
-				view.set_encoding('utf-8')
-				view.insert(edit,0,ee.mypage.getWikiText().decode('utf-8'))
-				view.end_edit(edit)
-	# def on_pre_save(self, view):
-		# params = {}
-		# params['text'] = view.substr(sublime.Region(0,view.size()))
-		# global site
-		# global mypage
-		# site.login('Sacchi Francesco', 'aA!tIrih1')
-		# mypage.edit(params)
-		# print "saved"
-
-
-
-		
+			ee.load(view)
